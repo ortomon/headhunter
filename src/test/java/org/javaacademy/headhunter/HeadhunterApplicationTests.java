86855ec03c59e70e1141849d36359278e82603fe
alina <art.alina73@mail.ru>
2024-04-04 15:40:12 +0500

    Initial commit

---
+++ src/test/java/org/javaacademy/headhunter/HeadhunterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaacademy.headhunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadhunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
